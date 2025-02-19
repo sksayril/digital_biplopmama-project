8670ece3f768a3e37bff07a1134a47e6316ce24d
sksayril <sksayril123@gmail.com>
2025-02-19 12:37:59 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { motion, useScroll, useSpring } from 'framer-motion';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Gallery from './pages/Gallery';
+import Contact from './pages/Contact';
+
+function App() {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
+
+  return (
+    <Router>
+      <div className="relative">
+        <motion.div
+          className="fixed top-0 left-0 right-0 h-1 bg-orange-500 origin-left z-50"
+          style={{ scaleX }}
+        />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
